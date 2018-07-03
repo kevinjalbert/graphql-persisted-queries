ca1a52151b2dac07954eca834a332214db454a6a
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2018-07-02 23:12:57 -0400

    Refactor React Application to use Persisted Queries

--- react-graphql/src/index.js
+++ react-graphql/src/index.js
@@ -5,12 +5,15 @@ import registerServiceWorker from './registerServiceWorker';
 import { ApolloClient, InMemoryCache } from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import { createHttpLink } from 'apollo-link-http';
+import { createPersistedQueryLink } from 'apollo-link-persisted-queries';
 
 import App from './components/App';
 import './index.css';
 
 const client = new ApolloClient({
-  link: createHttpLink({ uri: 'http://localhost:5000/graphql' }),
+  link: createPersistedQueryLink().concat(
+    createHttpLink({ uri: 'http://localhost:5000/graphql' })
+  ),
   cache: new InMemoryCache(),
 });
 
